7c93d22da3b03154d06f8e124f030603a5d11057
NathanOnCodes <contatonathancf@gmail.com>
2026-04-25 19:35:50 -0300

    feat: migracao upd User

--- administrativo/models.py
+++ administrativo/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+import uuid
+
+
+def caminho_imagem_capa(instance, filename):
+    return f'capas/{instance.id}/{filename}'
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    avatar = models.ImageField(upload_to=caminho_imagem_capa, blank=True, null=True)
+
+    @property
+    def nome_completo(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    @property
+    def path_avatar(self):
+        return self.avatar.url if self.avatar else None
+    
