d8be0a4657c9676f6eec339e06849174f844bcb2
NathanOnCodes <contatonathancf@gmail.com>
2026-04-25 20:52:53 -0300

    feat: detalhe de qualquer publicacao

--- administrativo/admin.py
+++ administrativo/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.html import format_html
+from administrativo.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'nome_completo', 'avatar_preview', 'is_staff')
+    
+    fieldsets = UserAdmin.fieldsets + (
+        ('Avatar', {
+            'fields': ('avatar',),
+        }),
+    )
+    
+    def nome_completo(self, obj):
+        return obj.nome_completo or obj.username
+    nome_completo.short_description = 'Nome'
+    
+    def avatar_preview(self, obj):
+        if obj.avatar:
+            return format_html('<img src="{}" width="32" height="32" style="border-radius: 50%;" />', obj.avatar.url)
+        return "📷"
+    avatar_preview.short_description = 'Avatar'
+    
