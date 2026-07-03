67e9697927755c8f967f2bf5de79c54900b10f5b
NathanOnCodes <contatonathancf@gmail.com>
2026-07-03 19:45:02 -0300

    Remove pages/oracoes.vue

--- pages/oracoes.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <PublicLayout>
-    <div class="mx-auto max-w-3xl px-6 py-20 text-center">
-      <h1 class="font-serif text-4xl text-slate-900">Orações</h1>
-      <p class="mt-4 text-slate-500">Em breve.</p>
-    </div>
-  </PublicLayout>
-</template>
